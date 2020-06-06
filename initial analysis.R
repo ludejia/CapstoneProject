a282505205068a3ec93150583e91b9eb95961afc
ludejia <ludejia@gmail.com>
2020-06-06 19:05:33 -0400

    update Jun 6

--- initial analysis.R
+++ initial analysis.R
@@ -39,7 +39,7 @@
 # Longtitude: Self explanitory
 
 
-house=read.csv('/Users/dejialu/Documents/Data Science/Ryerson/CKME136/Project/data/Melbourne_housing_FULL.csv')
+house=read.csv('data/Melbourne_housing_FULL.csv')
 str(house)
 sapply(house,function(x){sum(is.na(x))})
 summary(house)
@@ -48,6 +48,9 @@ table(house$Suburb)
 table(house$Regionname)
 
 library(ggmap)
+options(scipen=999)
+
+
 
 #for gg map you will need to use you google cloud api project key.
 #here is the tutorial https://developers.google.com/maps/documentation/embed/get-api-key
@@ -61,5 +64,313 @@ test=ggmap(map)
 house=read.csv('/Users/dejialu/Documents/Data Science/Ryerson/CKME136/Project/data/Melbourne_housing_FULL.csv')
 test+ geom_point(data = house, aes(x = house$Longtitude, y = house$Lattitude), 
                  color ='blue', size = 0.1,alpha=0.20)
+
+(house)
+
+str(house)
+#visualize the missing values
+
+sapply(house,function(x){sum(is.na(x))})
+
+library(visdat)
+vis_miss(house)
+
+sapply(house,function(x){sum(is.na(x))})
+
+missing=sapply(house,function(x){sum(is.na(x))})
+missing=as.data.frame(missing,row.names = NULL)
+missing$var=colnames(house)
+missing
+missing$missing=missing$missing/nrow(house)
+missing=missing[order(missing$missing),]
+missing
+ggplot(missing)+geom_bar(aes(x=reorder(var, missing),y=missing),
+                         stat = "identity", fill='grey',)+
+        coord_flip()
+
+
+upset(airquality,nsets = 3, number.angles = 30)
+upset(house, sets = c("Bathroom", "Car", "BuildingArea", "YearBuilt"),nsets = 6, number.angles = 30)
+
+test=data.frame(a=c(1,1,NA,NA,1),b=c(NA,1,1,1,1),c=c(1,1,NA,1,1))
+test
+upset(test)
+
+?upset
+houseNoNA=na.omit(house)
+vis_miss(houseNoNA)
+
+gg_misvar(airquality)
+
+tmp <- mtcars
+
+tmp$car <- rownames(mtcars)
+list <- lapply(colnames(mtcars), function(x) {
+        tmp[tmp[[x]] > median(tmp[[x]]), "car"]
+})
+names(list) <- colnames(mtcars)
+
+
+fromList(list)
+class(fromList(list))
+
+upset(fromList(list))
+
+#datamanipulation
+#drop address
+
+house=read.csv('data/Melbourne_housing_FULL.csv')
+#house$Suburb
+dim(house)
+table(house$Suburb)
+sum(is.na(house$Suburb))
+table(house$Suburb, useNA ='always')
+
+#house$address to be removed
+#str(house)
+#house=house[,-2]
+
+#recategory room type
+house$Type=as.character(house$Type)
+house[house$Type=='u',]$Type='apartment'
+house[house$Type=='h',]$Type='house'
+house[house$Type=='t',]$Type='townhouse'
+house$Type=as.factor(house$Type)
+
+#remove missing values of the houses
+house=house[!is.na(house$Price),]
+
+#Rename category method
+table(house$Method, useNA ='always')
+house$Method=as.character(house$Method)
+house$Method[house$Method=='PI']='property passed in'
+house$Method[house$Method=='S']='property sold'
+house$Method[house$Method=='SA']='sold after auction'
+house$Method[house$Method=='SP']='property sold prior'
+house$Method[house$Method=='VB']='vendor bid'
+house$Method=as.factor(house$Method)
+
+# convert column date to date format
+house$Date
+table(house$Date, useNA ='always')
+class(house$Date)
+library(lubridate)
+house$Date=dmy(house$Date)
+library(ggplot2)
+theme_set(theme_bw())
+
+# converdistance to numeric and change colname
+house$Distance=as.numeric(house$Distance)
+colnames(house)[9]='distance_to_CBD'
+
+
+#rooms
+house$Rooms
+table(house$Rooms)
+sum(is.na(house$Rooms))
+table(house$Rooms, useNA ='always')
+
+library(ggplot2)
+theme_set(theme_bw())
+plot=ggplot(house)
+plot+geom_bar(aes(x=house$Rooms), fill='blue')+
+        coord_cartesian(xlim=c(0,16),)+ 
+        scale_x_continuous(breaks=seq(0, 16, 1))+
+        labs(x='Number of Room')
+
+#house Type
+#h - house,cottage,villa, semi,terrace; 
+#u - unit, duplex;
+#t - townhouse; 
+house$Type
+sum(is.na(house$Type))
+table(house$Type, useNA ='always')
+
+g+geom_bar(aes(x=house$Type),fill='blue',width = 0.5)+
+        labs(x='house type')
+
+
+#house$Price
+
+summary(house$Price)
+g=ggplot(house)
+g+geom_histogram(aes(x=house$Price),
+                 binwidth = 20000, fill='blue')+
+        labs(title="histogram for house price", x='price')+
+        coord_cartesian(xlim=c(0,4000000),)
+
+g+geom_histogram(aes(x=house$Price, fill=house$Type),
+                 binwidth = 20000)+
+        labs(title="histogram for house price", x='price')+
+        coord_cartesian(xlim=c(0,4000000),)
+
+
+
+#house$Method        
+table(house$Method, useNA ='always')
+g+geom_bar(aes(x=house$Method), fill='blue',
+              width = 0.5)+
+        theme(axis.text.x = element_text(angle = 30,
+                                         vjust = 0.5))
+        
+
+#house$SellerG
+#remove
+
+#houes$date
+
+df_avghouseprice=aggregate(house$Price~house$Date,house,mean)
+class(df_avghouseprice)
+colnames(df_avghouseprice)=c('Date','average sold price')
+plot_date=ggplot(df_avghouseprice)
+plot_date+geom_line(aes(x=df_avghouseprice$Date,y=df_avghouseprice$`average sold price`),
+                    color="red")+
+        labs(x='date', y='average sold price')
+
+
+house$Distance
+
+summary(house$distance_to_CBD)
+g=ggplot(house)
+g+geom_histogram(aes(x=house$distance_to_CBD),
+                 col='black',fill='grey')+
+        labs(title="histogram for distance_to_CBD", x='Distance to CBD(km)')
+
+
+#postal code only one postal code is missing
+str(house)
+str(house$Postcode)
+table(house$Postcode, useNA ='always')
+sum(is.na(house$Postcode))
+
+house[house$Postcode=='#N/A',]
+
+length(unique(house$Postcode))
+length(unique(house$Suburb))
+
+
+#number of Bedroom
+#drop as room has the data without any missing value
+table(house$Bedroom2, useNA ='always')
+df_bedroom=as.data.frame(table(house$Bedroom2, useNA ='always'))
+colnames(df_bedroom)=c('number of bedroom','count')
+df_bedroom
+g_df_bedroom=ggplot(df_bedroom)
+g_df_bedroom+geom_bar(aes(x=df_bedroom$`number of bedroom`, y=df_bedroom$count),
+                      stat = "identity",
+                 col='black',fill='grey')+
+        labs(title="Histogram for the number of bedroom", x='number of bedroom',
+             y='count')
+
+#house$car
+table(house$Car,useNA ='always')
+
+sapply(house,function(x){sum(is.na(x))})
+nrow(house[is.na(house$Bedroom2),])
+nrow(house[is.na(house$Bathroom),])
+nrow(house[is.na(house$Bedroom2)&is.na(house$Bathroom),])
+
+table(house$Bathroom, useNA ='always')
+
+
+
+
+
+#number of parking,
+
+table(house$Car,useNA ='always')
+
+
+
+sum(house[!is.na(house$Bedroom2),]$Rooms==house[!is.na(house$Bedroom2),]$Bedroom2)
+nrow(house[is.na(house$Car),])
+nrow(house[is.na(house$Landsize),])
+nrow(house[is.na(house$Car)&is.na(house$Landsize),])
+     
+
+
+df_car=as.data.frame(table(house$Car, useNA ='always'))
+colnames(df_car)=c('number of parking','count')
+df_car
+g_df_car=ggplot(df_car)
+g_df_car+geom_bar(aes(x=df_car$`number of parking`, y=df_car$count),
+                      stat = "identity",
+                      col='black',fill='grey')+
+        labs(title="Histogram for the number of parking", x='number of parking',
+             y='count')
+
+
+#bathroom
+table(house$Bathroom,useNA ='always')
+
+#just impute from room
+nrow(house[is.na(house$Bathroom)&is.na(house$Bedroom2),])
+
+#Landsize & building area
+#for townhouse and apartment landsize would be much bigger than building area so just use building area
+
+str(house)
+test=house[house$Type=='apartment' &! is.na(house$BuildingArea),]
+library(ggplot2)
+ggplot(house)+geom_histogram(aes(x=house$Landsize),binwidth = 20,
+                             fill='grey',col='black')+
+        coord_cartesian(xlim=c(0,600))
+
+test=house[house$Landsize>600,]
+test=test[order(-test$Landsize),]
+
+
+#
+table(house$YearBuilt,useNA ='always')
+
+
+df_year=as.data.frame(table(house$YearBuilt, useNA ='always'))
+colnames(df_year)=c('year','count')
+df_year
+
+as.numeric(as.character(df_year$year))
+
+df_year$year=as.numeric(as.character(df_year$year))
+df_year$year
+
+test=c(1,2,3,4,5,6,7,8,9,10)
+cut(test,c(0,3,7,10))
+?cut
+df_year$year50 <- cut(df_year$year, c(0, 1899,1949,1979,1999,2050),
+                      include.lowest = T,
+                      right = TRUE)
+df_year
+levels(df_year$year50) <- c("<1900", "1900~1949", "1950~1979", "1980~1999",'2000~2010')
+ggplot(df_year)+geom_bar(aes(x=df_year$year50, y=df_year$count),
+                         stat = "identity",
+                         fill='grey')+
+        labs(title="Histogram for year built", x='year',
+             y='count')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
